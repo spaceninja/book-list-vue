6cc9fa1ed3494a73fa391d02b86f7648e969279f
Scott Vandehey <fake-scott@spaceninja.com>
2021-08-08 13:18:39 -0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+  overrides: [
+    {
+      // HACK: https://github.com/vuejs/eslint-plugin-vue/issues/1355
+      files: ['**/*.html'],
+      rules: { 'vue/comment-directive': 'off' },
+    },
+  ],
+};
---
+++ README.md
@@ -0,0 +1,27 @@
+# Book List Vue
+
+Bootstrapped with Vite & Vue 3.
+
+## Project setup
+
+```
+yarn
+```
+
+### Compiles and hot-reloads for development
+
+```
+yarn start
+```
+
+### Compiles and minifies for production
+
+```
+yarn build
+```
+
+### Lints and fixes files
+
+```
+yarn lint
+```
