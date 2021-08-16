160983fdbc034007e3d39b9130c9828ede9897c4
Scott Vandehey <fake-scott@spaceninja.com>
2021-08-15 22:06:19 -0700

    Add BookCard Component (#35)
    This PR adds the new BookCard component, for displaying the details of
    a single book.
    
    Fixes #31

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,15 +4,16 @@
   <div v-else>
     <h2>Book List</h2>
     <button type="button" @click="enterAddBookMode">Add New Book</button>
-    <ul>
+    <ol>
       <li v-if="isLoading">Loading…</li>
       <li v-else-if="!allBooks.length">No books found</li>
-      <li v-for="book in allBooks" :key="book.id">
-        {{ book.id }} - {{ book.isbn }} - {{ book.title }}
-        <button type="button" @click="enterEditBookMode(book)">Edit</button>
-        <button type="button" @click="deleteBook(book)">Delete</button>
-      </li>
-    </ul>
+      <BookCard
+        v-for="book in allBooks"
+        :key="book.isbn"
+        v-bind="book"
+        :book="book"
+      />
+    </ol>
     <BookDev v-if="isDev" />
   </div>
 </template>
@@ -25,11 +26,10 @@ import {
   editMode,
   fetchBooks,
   enterAddBookMode,
-  enterEditBookMode,
-  deleteBook,
 } from '../composables/useBook';
 import BookDev from './BookDev.vue';
 import BookEdit from './BookEdit.vue';
+import BookCard from './BookCard.vue';
 
 const isDev = import.meta.env.DEV;
 
