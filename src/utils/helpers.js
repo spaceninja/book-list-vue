83db5e691e0323c6c4909c3bf0702258451f448d
Scott Vandehey <fake-scott@spaceninja.com>
2021-08-21 14:01:38 -0700

    Fix Layout Bug
    This PR fixes a bug where the Book Card contents could overflow at
    narrow widths because the book title and series name did not have a
    space between them. It also adds a widon't helper to prevent the
    series number displaying as a typographic widow.
    
    Fixes #49

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -18,3 +18,17 @@ export const getParameterByName = (name, url) => {
   if (!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
 };
+
+/**
+ * Widon't
+ *
+ * Replaces last space with a non-breaking space to avoid typographical widows.
+ *
+ * @see http://justinhileman.info/article/a-jquery-widont-snippet/
+ *
+ * @param {string} string
+ * @returns string
+ */
+export const widont = (string) => {
+  return string.replace(/\s([^\s<]+)\s*$/, '\xa0$1');
+};
