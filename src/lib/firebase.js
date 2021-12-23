7ccd234212cda94f2604b5ede38fb01dd12ce56d
Scott Vandehey <fake-scott@spaceninja.com>
2021-12-23 15:11:11 -0800

    use environment variables

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,15 +2,14 @@
 import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
-// TODO: move to .env
 const firebaseConfig = {
-  apiKey: 'AIzaSyA-5tojAEBlkSLR-xwNFALegJw_A5Ch-WY',
-  authDomain: 'spaceninja-book-list-dev.firebaseapp.com',
-  databaseURL: 'https://spaceninja-book-list-dev.firebaseio.com',
-  projectId: 'spaceninja-book-list-dev',
-  storageBucket: 'spaceninja-book-list-dev.appspot.com',
-  messagingSenderId: '340949330740',
-  appId: '1:340949330740:web:9c8bb2f8fe83b8242e1361',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
