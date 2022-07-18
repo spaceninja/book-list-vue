d94fff1a5fe17c4d24fe174c3509c6ff3012ea55
Scott Vandehey <fake-scott@spaceninja.com>
2022-07-18 09:28:01 -0700

    testing fix

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    // https://stackoverflow.com/a/72608494
+    customExportConditions: ['node', 'node-addons'],
+  },
   transform: {
     '^.+\\.js$': 'babel-jest',
     '^.+\\.vue$': '@vue/vue3-jest',
